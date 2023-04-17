2fd889e88176298d511318316e2feccb66bdfc40
Ali_Labadi <ali.mohammed.labadi@gmail.com>
2023-04-17 17:32:43 +0300

    finished code for showing items in ul

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const todoInput = document.querySelector("#todo-input")
 const addBtn = document.querySelector(".add-btn")
 const ul = document.querySelector("ul")
 
-addBtn.addEventListener('click', () => {
-    const taskLi = document.createElement("li")
-    taskLi.append(todoInput.value)
-    ul.append(taskLi)
-    todoInput.value = ""
+todoObjs = {}
+
+addBtn.addEventListener("click", function() {
+    const todoInputValue = todoInput.value
+    const nextKey = Object.keys(todoObjs).length + 1 // generate next key
+    todoObjs[nextKey] = todoInputValue // add new key-value pair to object
+    console.log(todoObjs) // log updated object to console
+    todoInput.value = "" // clear the input after adding
+    appendTodoObjsToUl() // showing todo items in html with function
 })
+
+function appendTodoObjsToUl() {
+    ul.innerHTML = "" // clearing html before adding to the list
+    for (let key in todoObjs) { // showing the todoObjs in html
+        const li = document.createElement('li')
+        li.append(todoObjs[key])
+        ul.append(li)
+    }
+}
